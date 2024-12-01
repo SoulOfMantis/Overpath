5448d41ceefab64a1162352a05ebe3ebece98c6e
Артемий Данченко <artemferz@icloud.com>
2024-12-01 13:49:41 +0300

    full map with scripts

---
+++ Overpath/Assets/sprites/door.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "door", menuName = "Scriptable Objects/door")]
+public class door : ScriptableObject
+{
+puvlic gameObject VictoryMenu;
+ public void function Interacted(VictoryMenu);
+ {
+VictoryMenu.setactive(true)
+ }
+}
