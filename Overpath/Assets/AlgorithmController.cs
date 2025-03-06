c192ef7b1b011c48b247ca0e5f101f2b411f9e18
Андрей Тимофеев <timo@sfedu.ru>
2025-03-06 23:17:18 +0300

    CommandPanels Implemented
    Co-Authored-By: kenyf7 <115103255+kenyf7@users.noreply.github.com>

--- Overpath/Assets/AlgorithmController.cs
+++ Overpath/Assets/AlgorithmController.cs
@@ -2,30 +2,30 @@ using UnityEngine;
 public class AlgorithmController : MonoBehaviour
 {
     public CommandBlock[] commandBlocks;
+    public CommandPanel[] commandPanels;
     public RobotController robotController;
     public Player player;
     private int currentLine = 0;
 
-    public void Update()
+    void Start()
     {
     }
     public void ExecuteCurrentCommand()
     {
         if (currentLine < commandBlocks.Length)
-        {
-            commandBlocks[currentLine].Execute(ref currentLine, robotController);
-            player.MyTurn = true;
-        }
-    }
-
-    public void UpdateIfCondition(string blockName, bool newCondition)
-    {
-        foreach (var block in commandBlocks)
-        {
-            if (block.blockName == blockName && block is IfBlock ifBlock)
             {
-                ifBlock.condition = newCondition;
+                commandBlocks[currentLine].Execute(ref currentLine, robotController);
             }
-        }
+        player.MyTurn = true;
+    }
+    
+    public void SwapBlocksAndPanels(int indexA, int indexB)
+    {
+        (commandBlocks[indexA], commandBlocks[indexB]) = (commandBlocks[indexB], commandBlocks[indexA]);
+
+        (commandPanels[indexA], commandPanels[indexB]) = (commandPanels[indexB], commandPanels[indexA]);
+
+        commandPanels[indexA].BlockNumber = indexA;
+        commandPanels[indexB].BlockNumber = indexB;
     }
 }
