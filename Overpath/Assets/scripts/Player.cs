0a899ac09a62d005021915cf9c8c0a18988299bb
Delf1nchik <artemferz@icloud.com>
2025-05-01 23:57:02 +0300

    Bind Escape

--- Overpath/Assets/scripts/Player.cs
+++ Overpath/Assets/scripts/Player.cs
@@ -1,9 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Player : Actor
 {
     public GameObject GameOver;
+    public GameObject PauseMenuScreen;
     public Vector3Int dir = Vector3Int.down; // Более явное начальное значение
     public bool MyTurn = true;
     public SpriteRenderer spriteRenderer;
@@ -39,6 +41,7 @@ public class Player : Actor
             if (Input.GetKeyDown(KeyCode.D)) Move(Vector3Int.right);
             if (Input.GetKeyDown(KeyCode.F)) Interact();
             if (Input.GetKeyDown(KeyCode.Space)) EndOfTurn();
+            if (Input.GetKeyDown(KeyCode.Escape)) PauseMenuScreen.gameObject.SetActive(!PauseMenuScreen.gameObject.activeSelf);
         }
     }
 
