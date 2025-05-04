a3c22da2ced3d03efd5f62496d5adb4f74589571
Андрей Тимофеев <timo@sfedu.ru>
2025-05-04 15:46:19 +0300

    v1.0.1

--- Overpath/Assets/scripts/IntBlockInputField.cs
+++ Overpath/Assets/scripts/IntBlockInputField.cs
@@ -7,19 +7,35 @@ public class IntBlockInputField : MonoBehaviour
     public IntBlock LinkedBlock;
     public TerminalUI terminalUI;
 
+    void Start()
+    {
+        Field.onSubmit.AddListener(ValueChange);
+        Field.onValueChanged.AddListener(PaintText);
+    }
+
     void OnEnable()
     {
         Field.text = LinkedBlock.n.ToString();
-        Field.onSubmit.AddListener(ValueChange);
+        Field.textComponent.color = Color.black;
+
     }
     public void ValueChange(string NewText)
     {
         if (int.TryParse(NewText, out int NewN))
         {
             if ((NewN != LinkedBlock.n) && (NewN > 0) && terminalUI.Algorithm.ChangeOfAlgorithm())            
-                LinkedBlock.n = NewN;
+            {
+                LinkedBlock.n = NewN % 4;
+                Field.textComponent.color = Color.black;
+            }
         }
     }
+
+    void PaintText(string Text)
+    {
+        Field.textComponent.color = Color.red;
+    }
+
 }
 
 // public class InternalIntBlockInputField : IntBlockInputField
